ea57847736bee56d3df19757180b98897447b446
Anderson Gabriel <101525486+andgabx@users.noreply.github.com>
2024-06-05 18:12:33 -0300

    :fix: Atualizando código Arduino

--- alfabraille.ino
+++ alfabraille.ino
@@ -9,19 +9,24 @@ Servo servo[6];
 String speechToText;
 
 void setup() {
-  servo[0].attach(5);
+  servo[0].attach(3);
   servo[1].attach(5);
-  servo[2].attach(5);
-  servo[3].attach(5);
-  servo[4].attach(5);
-  servo[5].attach(5);
+  servo[2].attach(6);
+  servo[3].attach(9);
+  servo[4].attach(10);
+  servo[5].attach(11);
+  
+  for(int i=0; i<6; i++){
+    servo[i].write(20);
+  }
+
   Serial.begin(9600);
 }
 
 void moverPinos(char* posicoes){
   
-  int sobe = 90;
-  int desce = 10;
+  int sobe = 80;
+  int desce = 20;
   
   for(int i=0; i<6; i++){
     if(posicoes[i]=='1'){
